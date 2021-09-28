e588c6c4a3fd84c6ab3c77596b2b1662fae387d3
cathngo <cngo1080@gmail.com>
2021-09-28 21:54:08 +1000

    Added random generator and more units

--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/ConvertUnits.java
+++ app/src/main/java/au/edu/unsw/infs3634/covid19tracker/ConvertUnits.java
@@ -34,19 +34,183 @@ public class ConvertUnits extends AppCompatActivity {
             txtOriginalNumber.setText(originalNumber + "cm");
             txtTo.setText("M");
             txtFrom.setText("CM");
-            convertToMetre(originalNumber);
-        } else {
+            convertCentimetresToMetre(originalNumber);
+        } else if (conversion_type.equals("m_to_cm")){
             txtTo.setText("CM");
             txtFrom.setText("M");
             txtOriginalNumber.setText(originalNumber + "m");
-            convertToCentimetres(originalNumber);
+            convertMetresToCentimetres(originalNumber);
+        } else if (conversion_type.equals("cm_to_mm")) {
+            txtTo.setText("MM");
+            txtFrom.setText("CM");
+            txtOriginalNumber.setText(originalNumber + "cm");
+            convertCentimetresToMillimetres(originalNumber);
+        } else if (conversion_type.equals("mm_to_cm")){
+            txtTo.setText("CM");
+            txtFrom.setText("MM");
+            txtOriginalNumber.setText(originalNumber + "mm");
+            convertMillimetresToCentimetres(originalNumber);
+        } else if (conversion_type.equals("km_to_m")){
+            txtFrom.setText("KM");
+            txtTo.setText("M");
+            txtOriginalNumber.setText(originalNumber + "km");
+            convertKilometresToMetres(originalNumber);
+        } else if (conversion_type.equals("m_to_km")){
+            txtFrom.setText("M");
+            txtTo.setText("KM");
+            txtOriginalNumber.setText(originalNumber + "m");
+            convertMetresToKilometres(originalNumber);
+        } else if (conversion_type.equals("cm_to_km")){
+            txtFrom.setText("CM");
+            txtTo.setText("KM");
+            txtOriginalNumber.setText(originalNumber + "cm");
+            convertCentimetresToKilometres(originalNumber);
+        } else if (conversion_type.equals("km_to_cm")) {
+            txtFrom.setText("KM");
+            txtTo.setText("CM");
+            txtOriginalNumber.setText(originalNumber + "km");
+            convertKilometresToCentimetres(originalNumber);
+        } else if (conversion_type.equals("m_to_mm")) {
+            txtFrom.setText("M");
+            txtTo.setText("MM");
+            txtOriginalNumber.setText(originalNumber + "m");
+            convertMetresToMillimetres(originalNumber);
+        } else if (conversion_type.equals("mm_to_m")) {
+            txtFrom.setText("MM");
+            txtTo.setText("M");
+            txtOriginalNumber.setText(originalNumber + "mm");
+            convertMillimetresToMetres(originalNumber);
+        } else if (conversion_type.equals("mm_to_km")) {
+            txtFrom.setText("MM");
+            txtTo.setText("KM");
+            txtOriginalNumber.setText(originalNumber + "mm");
+            convertMillimetresToKilometres(originalNumber);
+        } else if (conversion_type.equals("km_to_mm")) {
+            txtFrom.setText("KM");
+            txtTo.setText("MM");
+            txtOriginalNumber.setText(originalNumber + "km");
+            convertKilometresToMillimetres(originalNumber);
         }
 
+    }
+    public void convertKilometresToMillimetres(String originalNumber) {
+        double initialNumber = Double.parseDouble(originalNumber);
 
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal multiplier = new BigDecimal(1000000);
+        BigDecimal res = bdNumber.multiply(multiplier).stripTrailingZeros();
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        txtConvertedNumber.setText(res.toPlainString() + "mm");
+
+    }
+    public void convertMillimetresToKilometres(String originalNumber){
+        double initialNumber = Double.parseDouble(originalNumber);
+        //double convertedNumber = (double) initialNumber /100;
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal divisor = new BigDecimal(1000000);
+        BigDecimal res = bdNumber.divide(divisor);
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        //txtConvertedNumber.setText(originalNumber + "cm = " + res.toString() + "m");
+        txtConvertedNumber.setText(res.toString() + "km");
+
+    }
+    public void convertMillimetresToMetres(String originalNumber){
+        double initialNumber = Double.parseDouble(originalNumber);
+        //double convertedNumber = (double) initialNumber /100;
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal divisor = new BigDecimal(1000);
+        BigDecimal res = bdNumber.divide(divisor);
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        //txtConvertedNumber.setText(originalNumber + "cm = " + res.toString() + "m");
+        txtConvertedNumber.setText(res.toString() + "m");
+
+    }
+    public void convertMetresToMillimetres(String originalNumber) {
+        double initialNumber = Double.parseDouble(originalNumber);
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal multiplier = new BigDecimal(1000);
+        BigDecimal res = bdNumber.multiply(multiplier).stripTrailingZeros();
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        txtConvertedNumber.setText(res.toPlainString() + "mm");
+
+    }
+    public void convertCentimetresToKilometres(String originalNumber){
+        double initialNumber = Double.parseDouble(originalNumber);
+        //double convertedNumber = (double) initialNumber /100;
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal divisor = new BigDecimal(100000);
+        BigDecimal res = bdNumber.divide(divisor);
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        //txtConvertedNumber.setText(originalNumber + "cm = " + res.toString() + "m");
+        txtConvertedNumber.setText(res.toString() + "km");
 
     }
 
-    public void convertToMetre(String originalNumber){
+    public void convertKilometresToCentimetres(String originalNumber) {
+        double initialNumber = Double.parseDouble(originalNumber);
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal multiplier = new BigDecimal(100000);
+        BigDecimal res = bdNumber.multiply(multiplier).stripTrailingZeros();
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        txtConvertedNumber.setText(res.toPlainString() + "cm");
+
+    }
+
+    public void convertMetresToKilometres(String originalNumber){
+        double initialNumber = Double.parseDouble(originalNumber);
+        //double convertedNumber = (double) initialNumber /100;
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal divisor = new BigDecimal(1000);
+        BigDecimal res = bdNumber.divide(divisor);
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        //txtConvertedNumber.setText(originalNumber + "cm = " + res.toString() + "m");
+        txtConvertedNumber.setText(res.toString() + "km");
+
+    }
+
+
+    public void convertKilometresToMetres(String originalNumber) {
+        double initialNumber = Double.parseDouble(originalNumber);
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal multiplier = new BigDecimal(1000);
+        BigDecimal res = bdNumber.multiply(multiplier).stripTrailingZeros();
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        txtConvertedNumber.setText(res.toPlainString() + "m");
+
+    }
+
+    public void convertCentimetresToMetre(String originalNumber){
         double initialNumber = Double.parseDouble(originalNumber);
         //double convertedNumber = (double) initialNumber /100;
 
@@ -62,7 +226,7 @@ public class ConvertUnits extends AppCompatActivity {
 
     }
 
-    public void convertToCentimetres(String originalNumber) {
+    public void convertMetresToCentimetres(String originalNumber) {
         double initialNumber = Double.parseDouble(originalNumber);
 
         //convert to human math
@@ -73,6 +237,35 @@ public class ConvertUnits extends AppCompatActivity {
         //set text
         TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
         txtConvertedNumber.setText(res.toPlainString() + "cm");
+
+    }
+
+    public void convertCentimetresToMillimetres(String originalNumber) {
+        double initialNumber = Double.parseDouble(originalNumber);
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal multiplier = new BigDecimal(10);
+        BigDecimal res = bdNumber.multiply(multiplier).stripTrailingZeros();
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        txtConvertedNumber.setText(res.toPlainString() + "mm");
+
+    }
+
+    public void convertMillimetresToCentimetres(String originalNumber) {
+        double initialNumber = Double.parseDouble(originalNumber);
+
+        //convert to human math
+        BigDecimal bdNumber = BigDecimal.valueOf(initialNumber);
+        BigDecimal divisor = new BigDecimal(10);
+        BigDecimal res = bdNumber.divide(divisor);
+
+        //set text
+        TextView txtConvertedNumber = findViewById(R.id.txtConvertedNumber);
+        //txtConvertedNumber.setText(originalNumber + "cm = " + res.toString() + "m");
+        txtConvertedNumber.setText(res.toString() + "cm");
 
     }
 
--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private TextView txtLeftUnit;
     private TextView txtRightUnit;
@@ -25,12 +27,73 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //generate random number to select conversion type
+        Random r = new Random();
+        int low = 1;
+        int high = 12;
+        int result = r.nextInt(high-low) + low;
+        System.out.println("number is " + result);
+
         txtLeftUnit = findViewById(R.id.txtLeftUnit);
         txtRightUnit = findViewById(R.id.txtRightUnit);
         txtPrompt = findViewById(R.id.txtPrompt);
 
+        if (result == 1) {
+            txtLeftUnit.setText("CM");
+            txtRightUnit.setText("M");
+            txtPrompt.setText("Enter units in centimetres to convert");
+        } else if (result == 2) {
+            txtLeftUnit.setText("M");
+            txtRightUnit.setText("CM");
+            txtPrompt.setText("Enter units in metres to convert");
+        } else if (result == 3) {
+            txtLeftUnit.setText("CM");
+            txtRightUnit.setText("MM");
+            txtPrompt.setText("Enter units in centimetres to convert");
+        } else if (result == 4) {
+            txtLeftUnit.setText("MM");
+            txtRightUnit.setText("CM");
+            txtPrompt.setText("Enter units in millimetres to convert");
+        }  else if (result == 5) {
+            txtLeftUnit.setText("KM");
+            txtRightUnit.setText("M");
+            txtPrompt.setText("Enter units in kilometres to convert");
+        }  else if (result == 6) {
+            txtLeftUnit.setText("M");
+            txtRightUnit.setText("KM");
+            txtPrompt.setText("Enter units in metres to convert");
+        } else if (result == 7) {
+            txtLeftUnit.setText("CM");
+            txtRightUnit.setText("KM");
+            txtPrompt.setText("Enter units in centimetres to convert");
+        } else if (result == 8) {
+            txtLeftUnit.setText("KM");
+            txtRightUnit.setText("CM");
+            txtPrompt.setText("Enter units in kilometres to convert");
+        } else if (result == 9) {
+            txtLeftUnit.setText("M");
+            txtRightUnit.setText("MM");
+            txtPrompt.setText("Enter units in metres to convert");
+        } else if (result == 10) {
+            txtLeftUnit.setText("MM");
+            txtRightUnit.setText("M");
+            txtPrompt.setText("Enter units in millimetres to convert");
+        } else if (result == 11) {
+            txtLeftUnit.setText("MM");
+            txtRightUnit.setText("KM");
+            txtPrompt.setText("Enter units in millimetres to convert");
+        } else if (result == 12) {
+            txtLeftUnit.setText("KM");
+            txtRightUnit.setText("MM");
+            txtPrompt.setText("Enter units in kilometres to convert");
+        }
+
+
         //get units entered
         EditText editOriginalUnits = findViewById(R.id.editOriginalUnits);
+
+        /*
 
         //check drop down menu
         Spinner conversion = findViewById(R.id.spinnerConvert);
@@ -40,7 +103,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         conversion.setAdapter(adapter);
 
-        conversion.setOnItemSelectedListener(MainActivity.this);
+        //conversion.setOnItemSelectedListener(MainActivity.this);
+    */
 
         //convert button onclick
         Button btnConvert = findViewById(R.id.btnConvert);
@@ -54,15 +118,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     Intent intent = new Intent(MainActivity.this, ConvertUnits.class);
                     intent.putExtra("originalNumber", originalNumber);
                     //check conversion type
-                    if (txtLeftUnit.getText().equals("CM")) {
+                    if (result == 1) {
                         intent.putExtra("conversion", "cm_to_m");
-                    } else {
+                    } else if (result == 2){
                         intent.putExtra("conversion","m_to_cm");
+                    } else if (result == 3) {
+                        intent.putExtra("conversion", "cm_to_mm");
+                    } else if (result == 4 ) {
+                        intent.putExtra("conversion", "mm_to_cm");
+                    } else if (result == 5 ) {
+                        intent.putExtra("conversion", "km_to_m");
+                    } else if (result == 6 ) {
+                        intent.putExtra("conversion", "m_to_km");
+                    } else if (result == 7 ) {
+                        intent.putExtra("conversion", "cm_to_km");
+                    } else if (result == 8 ) {
+                        intent.putExtra("conversion", "km_to_cm");
+                    } else if (result == 9 ) {
+                        intent.putExtra("conversion", "m_to_mm");
+                    } else if (result == 10) {
+                        intent.putExtra("conversion", "mm_to_m");
+                    } else if (result == 11) {
+                        intent.putExtra("conversion", "mm_to_km");
+                    } else if (result == 12) {
+                        intent.putExtra("conversion", "km_to_mm");
                     }
                     //switch screen
                     startActivity(intent);
                 } else {
-                    Toast.makeText(MainActivity.this, "Please input units to be converted", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,
+                            "Please input units to be converted", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -73,13 +158,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         if (adapterView.getId() == R.id.spinnerConvert) {
             String conversion_type = adapterView.getItemAtPosition(i).toString();
             if (conversion_type.equals("cm to m")) {
-                txtLeftUnit.setText("CM");
-                txtRightUnit.setText("M");
-                txtPrompt.setText("Enter units in centimetres to convert");
+
             } else {
-                txtLeftUnit.setText("M");
-                txtRightUnit.setText("CM");
-                txtPrompt.setText("Enter units in metres to convert");
+
             }
         }
     }
